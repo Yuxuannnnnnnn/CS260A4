e4e86b696f0e07c8903eecec77a83a645dc512ee
Yuxuannnnnnnn <luoyuxuan43@gmail.com>
2021-08-07 13:15:59 +0800

    Update LogicSystem.h

--- cs260A4/cs260A4/logic/LogicSystem.h
+++ cs260A4/cs260A4/logic/LogicSystem.h
@@ -265,12 +265,9 @@ public:
 			{
 				//players need to know the gameObjectID of each gameObject
 				//push gameObjectID
-				messageList.push_back({ std::to_string(object.second).c_str(), 
-										std::to_string(object.second).size() + sizeof("\0")});
 
 				//players need to know the details of each gameObjects
 				GameObject& obj = gameFactory->getGameObject(object.second);
-
 
 			}
 
@@ -283,6 +280,41 @@ public:
 		}
 	}
 
+
+//Insert to MessageList---------------------
+
+	void InsertGameObject_MessageList(MessageList& messageList, const GameObject& object)
+	{
+		Insert_Vec2_MessageList(messageList, object.transform.position);
+		Insert_Vec2_MessageList(messageList, object.transform.scale);
+		Insert_Number_MessageList(messageList, object.transform.rotation);
+
+		Insert_Number_MessageList(messageList, object.mesh);
+		Insert_Vec3_MessageList(messageList, object.color);
+
+		Insert_Number_MessageList(messageList, object.obj_type);
+		Insert_Number_MessageList(messageList, object.playerIndex);
+	}
+
+
+	//function to convert a Vec3 to a string and then to a Message
+	//Insert the Mesage to the messageList
+	void Insert_Vec3_MessageList(MessageList& messageList, const Vector3& vec)
+	{
+		Insert_Number_MessageList(messageList, vec.x);
+		Insert_Number_MessageList(messageList, vec.y);
+		Insert_Number_MessageList(messageList, vec.z);
+	}
+
+	//function to convert a Vec2 to a string and then to a Message
+	//Insert the Mesage to the messageList
+	void Insert_Vec2_MessageList(MessageList& messageList, const Vector2& vec)
+	{
+		Insert_Number_MessageList(messageList, vec.x);
+		Insert_Number_MessageList(messageList, vec.y);
+	}
+
+
 	//function to convert an integer/float to a string and then to a Message
 	//Insert the Mesage to the messageList
 	template<typename type>
@@ -292,13 +324,38 @@ public:
 		std::to_string(number).size() + sizeof("\0") });
 	}
 
-	//function to convert a Vec2 to a string and then to a Message
-	//Insert the Mesage to the messageList
-	void Insert_Vec2(MessageList& messageList, Vector2& vec)
+
+//Extract From MessageList---------------------
+
+	void ExtractGameObject_MessageList(int index, MessageList& messageList, GameObject& object)
 	{
-		Insert_Number_MessageList(messageList, vec.x);
-		Insert_Number_MessageList(messageList, vec.y);
+		Extract_Vec2_MessageList(index, messageList, object.transform.position);
+		Extract_Vec2_MessageList(index, messageList, object.transform.scale);
+		Extract_Number_MessageList(index, messageList, object.transform.rotation);
+
+		Extract_Number_MessageList(index, messageList, object.mesh);
+		Extract_Vec3_MessageList(index, messageList, object.color);
+
+		Extract_Number_MessageList(index, messageList, object.obj_type);
+		Extract_Number_MessageList(index, messageList, object.playerIndex);
 	}
+
+
+	//function to extract an vec3 from the messageList
+	void Extract_Vec3_MessageList(int index, MessageList& messageList, Vector3& vec)
+	{
+		Extract_Number_MessageList(index, messageList, vec.x);
+		Extract_Number_MessageList(index, messageList, vec.y);
+		Extract_Number_MessageList(index, messageList, vec.z);
+	}
+
+	//function to extract an vec2 from the messageList
+	void Extract_Vec2_MessageList(int index, MessageList& messageList, Vector2& vec)
+	{
+		Extract_Number_MessageList(index, messageList, vec.x);
+		Extract_Number_MessageList(index, messageList, vec.y);
+	}
+
 
 	//function to extract an integer or float from the messageList
 	template<typename type>
@@ -312,19 +369,12 @@ public:
 
 		if (typeid(type) == typeid(int))
 		{
-			number = stoi(NumberString);
+			number = (type)stoi(NumberString);
 		}
 		else if (typeid(type) == typeid(float))
 		{
-			number = stof(NumberString);
+			number = (type)stof(NumberString);
 		}
-	}
-
-	//function to extract an vec2 from the messageList
-	void Extract_Vec2_MessageList(int index, MessageList& messageList, Vector2& vec)
-	{
-		Extract_Number_MessageList(index, messageList, vec.x);
-		Extract_Number_MessageList(index, messageList, vec.y);
 	}
 
 
