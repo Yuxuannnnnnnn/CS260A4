983c272531010e1641dfc5ab466998455b6bb744
satsey <54670828+satsey@users.noreply.github.com>
2021-08-11 00:53:15 +0800

    Create run.bat

---
+++ cs260A4/run.bat
@@ -0,0 +1,7 @@
+start cs260A4 localhost:1702 localhost:1701 localhost:1700 localhost:1703
+timeout(1)
+start cs260A4 localhost:1701 localhost:1700 localhost:1702 localhost:1703
+timeout(1)
+start cs260A4 localhost:1703 localhost:1701 localhost:1702 localhost:1700
+timeout(1)
+start cs260A4 localhost:1700 localhost:1701 localhost:1702 localhost:1703
